45cad1d6ace8ac3188573aadc058229361a72f28
tobias-mintlify <110702161+tobias-mintlify@users.noreply.github.com>
2023-01-21 18:31:52 -0800

    Do not catch getPage

--- client/src/lib/page.ts
+++ client/src/lib/page.ts
@@ -1,25 +1,15 @@
 import axios from 'axios';
 
 export const getPage = async (subdomain: string, path: string) => {
-  try {
-    const { data, status } = await axios.get(
-      `${process.env.API_ENDPOINT}/api/v2/internal/deployment/${subdomain}/static-props`,
-      {
-        params: {
-          path,
-          basePath: process.env.BASE_PATH,
-        },
-        headers: { Authorization: `Bearer ${process.env.ADMIN_TOKEN}` },
-      }
-    );
-    return { data, status };
-  } catch (err: any) {
-    const {
-      response: { data, status },
-    }: { response: { data: any; status: number } } = err;
-    return {
-      data,
-      status,
-    };
-  }
+  const { data, status } = await axios.get(
+    `${process.env.API_ENDPOINT}/api/v2/internal/deployment/${subdomain}/static-props`,
+    {
+      params: {
+        path,
+        basePath: process.env.BASE_PATH,
+      },
+      headers: { Authorization: `Bearer ${process.env.ADMIN_TOKEN}` },
+    }
+  );
+  return { data, status };
 };
